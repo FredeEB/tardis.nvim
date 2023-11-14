eec54b7455309b2c8fed8d1a201e687ea8fea4aa
Frede Braendstrup <frederikbraendstrup@gmail.com>
2023-11-14 23:47:56 +0100

    chore(util): clean-up

--- lua/tardis-nvim/util.lua
+++ lua/tardis-nvim/util.lua
@@ -1,40 +1,15 @@
 local M = {}
-local constants = require('tardis-nvim.constants')
 
-function M.trim_root_path(path, root)
-    return string.sub(path, string.len(root) + 2, string.len(path))
-end
-
-function M.force_delete_buffer(buffer)
-    return function() vim.api.nvim_buf_delete(buffer, { force = true }) end
-end
-
-function M.build_commit_buffer_name(commit)
-    return string.format('%s: #%s', constants.name_prefix, commit)
-end
-
-function M.build_buffer_name(filename, commit)
-    return string.format('%s: %s #%s', constants.name_prefix, filename, commit)
-end
-
-function M.close_session(origin, buffer)
-    return function ()
-        vim.api.nvim_win_set_buf(0, origin)
-        M.force_delete_buffer(buffer)()
+function M.dirname(path)
+    local str = string.gsub(path, '(.*/)(.*)', '%1')
+    if str == path then
+        return '.'
     end
+    return str
 end
 
-function M.goto_buffer(index, buffers)
-    return function()
-        local target = buffers[index].fd
-        local current_pos = vim.api.nvim_win_get_cursor(0)
-        local target_line_count = vim.api.nvim_buf_line_count(target)
-        if current_pos[1] >= target_line_count then
-            current_pos[1] = target_line_count
-        end
-        vim.api.nvim_win_set_buf(0, target)
-        vim.api.nvim_win_set_cursor(0, current_pos)
-    end
+function M.basename(path)
+    return string.gsub(path, '(.*/)(.*)', '%2')
 end
 
 return M
